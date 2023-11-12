d2108421c7101a7ece12abb1305faff34046b737
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-11-12 08:36:41 -0600

    Add Apis to Upvote and Comment on posts

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify, session
-from app.models import User
+from app.models import User, Post, Comment, Vote
 from app.db import get_db
 import sys
 
@@ -50,7 +50,53 @@ def login():
     print(sys.exc_info()[0])
     if user.verify_password(data['password']) == False:
       return jsonify(message = 'Incorrect credentials'), 400
-    session.clear()
-    session['user_id'] = user.id
-    session['loggedIn'] = True
+  session.clear()
+  session['user_id'] = user.id
+  session['loggedIn'] = True
   return jsonify(id = user.id)
+
+
+@bp.route('/comments', methods=['POST'])
+def comment():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        # Create a new comment
+        newComment = Comment(
+            comment_text=data['comment_text'],
+            post_id=data['post_id'],
+            user_id=session.get('user_id')
+        )
+
+        db.add(newComment)
+        db.commit()
+
+    except:
+        print(sys.exc_info()[0])
+        db.rollback()
+        return jsonify(message='Comment failed'), 500
+
+    return jsonify(id=newComment.id)
+
+@bp.route('/posts/upvote', methods=['PUT'])
+def upvote():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # create a new vote with incoming id and session id
+    newVote = Vote(
+      post_id = data['post_id'],
+      user_id = session.get('user_id')
+    )
+
+    db.add(newVote)
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Upvote failed'), 500
+
+  return '', 204
