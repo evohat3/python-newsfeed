892d61f6edb0a26ae1786cc2646337ed398b3cf8
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-11-12 05:19:09 -0600

    finalize login and sessions

--- app/__init__.py
+++ app/__init__.py
@@ -14,11 +14,6 @@ def create_app(test_config=None):
     SECRET_KEY='super_secret_key'
   )
 
-
-  from app.routes import home
-  @app.route('/hello')
-  def hello():
-    return 'hello world'
 # register routes
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
